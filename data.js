783b2ac1e4a441638f0c4e857af0f7461d550673
Virapub <virapublishers@gmail.com>
2025-06-30 15:46:34 +0530

    Update data.js chatgpt

--- data.js
+++ data.js
@@ -1,17 +1,11 @@
-// data.js
 const data = {
     products: [
         { id: "philips-air-fryer", name: "Philips Air Fryer", price: "Up to ₹10,000", url: "https://amzn.to/4kgzH4R", image: "https://m.media-amazon.com/images/I/414ly0wsjYL._SL1000_.jpg", page: "products.html#philips-air-fryer" },
-        { id: "Sliding Tray", name: "Sliding Tray", price: "Under ₹1,000", url: "https://amzn.to/3SS1XQ4", image: "https://m.media-amazon.com/images/I/61rUIJtyEkL._UF1000,1000_QL80_FMwebp_.jpg", page: "products.html#Sliding Tray" }
-    { id: "Vacuum Sealer", name: "Vacuum Sealer", price: "Up to ₹3,000", url: "https://amzn.to/3SVcs54", image: "https://m.media-amazon.com/images/I/71QkgEBOZiL._UF894,1000_QL80_FMwebp_.jpg", page: "products.html#Vacuum Sealer" }
-    { id: "Mini Sealing Machine", name: "Mini Sealing Machine", price: "Under ₹5,00", url: "https://amzn.to/4k1PTqE", image: "https://m.media-amazon.com/images/I/71utCY3f-JL._UF894,1000_QL80_FMwebp_.jpg", page: "products.html#Mini Sealing Machine" }
-    { id: "Kitchen Mat", name: "Kitchen Mat", price: "Upto ₹1,000", url: "https://amzn.to/3HG0ry2", image: "https://m.media-amazon.com/images/I/619hMFvYSoL._UF894,1000_QL80_FMwebp_.jpg", page: "products.html#Kitchen Mat" }
-    { id: "Knife Sharpener", name: "Knife Sharpener", price: "Under ₹500", url: "https://amzn.to/45TExRW", image: "https://m.media-amazon.com/images/I/610WnGc6kPL._UF894,1000_QL80_FMwebp_.jpg", page: "products.html#Knife Sharpener" }
-    { id: "#", name: "#", price: "Up to ₹#", url: "https://amzn.to/#", image: "https://m.media-amazon.com/images/I/#.jpg", page: "products.html#product" }
-    { id: "#", name: "#", price: "Up to ₹#", url: "https://amzn.to/#", image: "https://m.media-amazon.com/images/I/#.jpg", page: "products.html#product" }
-    { id: "#", name: "#", price: "Up to ₹#", url: "https://amzn.to/#", image: "https://m.media-amazon.com/images/I/#.jpg", page: "products.html#product" }
-    
-    // अन्य प्रोडक्ट्स यहाँ जोड़ें
+        { id: "sliding-tray", name: "Sliding Tray", price: "Under ₹1,000", url: "https://amzn.to/3SS1XQ4", image: "https://m.media-amazon.com/images/I/61rUIJtyEkL._UF1000,1000_QL80_FMwebp_.jpg", page: "products.html#sliding-tray" },
+        { id: "vacuum-sealer", name: "Vacuum Sealer", price: "Up to ₹3,000", url: "https://amzn.to/3SVcs54", image: "https://m.media-amazon.com/images/I/71QkgEBOZiL._UF894,1000_QL80_FMwebp_.jpg", page: "products.html#vacuum-sealer" },
+        { id: "mini-sealing-machine", name: "Mini Sealing Machine", price: "Under ₹500", url: "https://amzn.to/4k1PTqE", image: "https://m.media-amazon.com/images/I/71utCY3f-JL._UF894,1000_QL80_FMwebp_.jpg", page: "products.html#mini-sealing-machine" },
+        { id: "kitchen-mat", name: "Kitchen Mat", price: "Up to ₹1,000", url: "https://amzn.to/3HG0ry2", image: "https://m.media-amazon.com/images/I/619hMFvYSoL._UF894,1000_QL80_FMwebp_.jpg", page: "products.html#kitchen-mat" },
+        { id: "knife-sharpener", name: "Knife Sharpener", price: "Under ₹500", url: "https://amzn.to/45TExRW", image: "https://m.media-amazon.com/images/I/610WnGc6kPL._UF894,1000_QL80_FMwebp_.jpg", page: "products.html#knife-sharpener" }
     ],
     mealPlans: {
         office: [
@@ -19,15 +13,14 @@ const data = {
             { id: "office-tuesday", name: "Boiled Eggs + Toast, Rice + Sambar + Curd, Roti + Veg Kurma", day: "Tuesday", page: "office-meal-plan.html#office-tuesday" },
             { id: "office-wednesday", name: "Upma + Chai, Chickpea Salad + Buttermilk, Lemon Rice + Pickle", day: "Wednesday", page: "office-meal-plan.html#office-wednesday" },
             { id: "office-thursday", name: "Bread + Avocado Spread, Veg Burrito Bowl + Curd, Dal Khichdi + Ghee", day: "Thursday", page: "office-meal-plan.html#office-thursday" },
-            { id: "office-friday", name: "Poha + Sprouts, Jeera Rice + Rajma, Chapati + Bhindi Fry", day: "Friday", page: "office-meal-plan.html#office-friday" },
+            { id: "office-friday", name: "Poha + Sprouts, Jeera Rice + Rajma, Chapati + Bhindi Fry", day: "Friday", page: "office-meal-plan.html#office-friday" }
         ],
         budget: [
             { id: "budget-monday", name: "Poha + Tea, Rice + Dal + Veg, Chapati + Mixed Sabzi", day: "Monday", page: "meal-prep.html#budget-monday" },
             { id: "budget-tuesday", name: "Idli + Chutney, Veg Pulao + Raita, Chapati + Paneer Bhurji", day: "Tuesday", page: "meal-prep.html#budget-tuesday" },
             { id: "budget-wednesday", name: "Upma + Filter Coffee, Khichdi + Pickle, Rice + Rajma", day: "Wednesday", page: "meal-prep.html#budget-wednesday" },
             { id: "budget-thursday", name: "Bread + Peanut Butter, Veg Sandwich + Juice, Chapati + Aloo Curry", day: "Thursday", page: "meal-prep.html#budget-thursday" },
-            { id: "budget-friday", name: "Oats + Banana, Lemon Rice + Curd, Roti + Chana Masala", day: "Friday", page: "meal-prep.html#budget-friday" },
-        ],
-    },
+            { id: "budget-friday", name: "Oats + Banana, Lemon Rice + Curd, Roti + Chana Masala", day: "Friday", page: "meal-prep.html#budget-friday" }
+        ]
+    }
 };
-
