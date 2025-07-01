24c31451ae790462eb9ccc49e3c0ecfbe2848057
Virapub <virapublishers@gmail.com>
2025-07-01 08:28:35 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,122 +1,81 @@
 const data = {
   products: [
+  {
+  "products": [
     {
-      id: "philips-air-fryer",
-      name: "Philips Air Fryer",
-      price: "Up to ₹10,000 / $119",
-      url: "https://amzn.to/4kgzH4R",
-      image: "https://m.media-amazon.com/images/I/414ly0wsjYL._SL1000_.jpg",
-      page: "products.html#philips-air-fryer"
+      "id": "philips-air-fryer",
+      "name": "Philips Air Fryer",
+      "price": "Up to ₹10,000 / $119",
+      "url": "https://amzn.to/4kgzH4R",
+      "image": "https://m.media-amazon.com/images/I/414ly0wsjYL._SL1000_.jpg",
+      "page": "products.html#philips-air-fryer"
     },
     {
-      id: "sliding-tray",
-      name: "Sliding Tray",
-      price: "Under ₹1,000 / $11.99",
-      url: "https://amzn.to/3SS1XQ4",
-      image: "https://m.media-amazon.com/images/I/61rUIJtyEkL._UF1000,1000_QL80_FMwebp_.jpg",
-      page: "products.html#sliding-tray"
+      "id": "sliding-tray",
+      "name": "Sliding Tray",
+      "price": "Under ₹1,000 / $11.99",
+      "url": "https://amzn.to/3SS1XQ4",
+      "image": "https://m.media-amazon.com/images/I/61rUIJtyEkL._UF1000,1000_QL80_FMwebp_.jpg",
+      "page": "products.html#sliding-tray"
     },
     {
-      id: "vaccum-sealer",
-      name: "Vacuum Sealer",
-      price: "Up to ₹3,000 / $35.99",
-      url: "https://amzn.to/3SVcs54",
-      image: "https://m.media-amazon.com/images/I/71QkgEBOZiL._UF894,1000_QL80_FMwebp_.jpg",
-      page: "products.html#vaccum-sealer"
+      "id": "vaccum-sealer",
+      "name": "Vacuum Sealer",
+      "price": "Up to ₹3,000 / $35.99",
+      "url": "https://amzn.to/3SVcs54",
+      "image": "https://m.media-amazon.com/images/I/71QkgEBOZiL._UF894,1000_QL80_FMwebp_.jpg",
+      "page": "products.html#vaccum-sealer"
     },
     {
-      id: "mini-sealing-machine",
-      name: "Mini Sealing Machine",
-      price: "Under ₹500 / $3.59",
-      url: "https://amzn.to/4k1PTqE",
-      image: "https://m.media-amazon.com/images/I/71utCY3f-JL._UF894,1000_QL80_FMwebp_.jpg",
-      page: "products.html#mini-sealing-machine"
+      "id": "mini-sealing-machine",
+      "name": "Mini Sealing Machine",
+      "price": "Under ₹500 / $3.59",
+      "url": "https://amzn.to/4k1PTqE",
+      "image": "https://m.media-amazon.com/images/I/71utCY3f-JL._UF894,1000_QL80_FMwebp_.jpg",
+      "page": "products.html#mini-sealing-machine"
     },
     {
-      id: "kitchen-mat",
-      name: "Kitchen Mat",
-      price: "Up to ₹1,000 / $8.39",
-      url: "https://amzn.to/3HG0ry2",
-      image: "https://m.media-amazon.com/images/I/619hMFvYSoL._UF894,1000_QL80_FMwebp_.jpg",
-      page: "products.html#kitchen-mat"
+      "id": "kitchen-mat",
+      "name": "Kitchen Mat",
+      "price": "Up to ₹1,000 / $8.39",
+      "url": "https://amzn.to/3HG0ry2",
+      "image": "https://m.media-amazon.com/images/I/619hMFvYSoL._UF894,1000_QL80_FMwebp_.jpg",
+      "page": "products.html#kitchen-mat"
     },
     {
-      id: "knife-sharpener",
-      name: "Knife Sharpener",
-      price: "Under ₹500 / $2.99",
-      url: "https://amzn.to/45TExRW",
-      image: "https://m.media-amazon.com/images/I/610WnGc6kPL._UF894,1000_QL80_FMwebp_.jpg",
-      page: "products.html#knife-sharpener"
+      "id": "knife-sharpener",
+      "name": "Knife Sharpener",
+      "price": "Under ₹500 / $2.99",
+      "url": "https://amzn.to/45TExRW",
+      "image": "https://m.media-amazon.com/images/I/610WnGc6kPL._UF894,1000_QL80_FMwebp_.jpg",
+      "page": "products.html#knife-sharpener"
     },
     {
-      id: "breakfast-station",
-      name: "3-in-1 Breakfast Maker",
-      price: "Up to ₹8,000 / $95.99",
-      url: "https://amzn.to/45K1VkQ",
-      image: "https://m.media-amazon.com/images/I/619zlfuWKgL._SL1001_.jpg",
-      page: "products.html#breakfast-station"
+      "id": "breakfast-station",
+      "name": "3-in-1 Breakfast Maker",
+      "price": "Up to ₹8,000 / $95.99",
+      "url": "https://amzn.to/45K1VkQ",
+      "image": "https://m.media-amazon.com/images/I/619zlfuWKgL._SL1001_.jpg",
+      "page": "products.html#breakfast-station"
     },
     {
-      id: "multi-cooker",
-      name: "Prestige Multi-Cooker 1.5L",
-      price: "Under ₹2,500 / $29.99",
-      url: "https://amzn.to/4kll3ZT",
-      image: "https://m.media-amazon.com/images/I/71OHF3Zl0bL._SL1500_.jpg",
-      page: "products.html#multi-cooker"
+      "id": "multi-cooker",
+      "name": "Prestige Multi-Cooker 1.5L",
+      "price": "Under ₹2,500 / $29.99",
+      "url": "https://amzn.to/4kll3ZT",
+      "image": "https://m.media-amazon.com/images/I/71OHF3Zl0bL._SL1500_.jpg",
+      "page": "products.html#multi-cooker"
     },
     {
-      id: "atomberg-mixer",
-      name: "Atomberg Zenova Mixer Grinder",
-      price: "Up to ₹7,000 / $84.99",
-      url: "https://amzn.to/4ex7lCf",
-      image: "https://m.media-amazon.com/images/I/81haDHz+llL._SL1500_.jpg",
-      page: "products.html#atomberg-mixer"
+      "id": "atomberg-mixer",
+      "name": "Atomberg Zenova Mixer Grinder",
+      "price": "Up to ₹7,000 / $84.99",
+      "url": "https://amzn.to/4ex7lCf",
+      "image": "https://m.media-amazon.com/images/I/81haDHz+llL._SL1500_.jpg",
+      "page": "products.html#atomberg-mixer"
     }
-  ],
-
-  mealPlans: {
-    office: [
-      {
-        id: "office-monday",
-        name: "Banana Smoothie + Oats, Quinoa + Stir-fry Veggies, Chapati + Palak Paneer",
-        day: "Monday",
-        page: "office-meal-plan.html#office-monday"
-      },
-      {
-        id: "office-tuesday",
-        name: "Boiled Eggs + Toast, Rice + Sambar + Curd, Roti + Veg Kurma",
-        day: "Tuesday",
-        page: "office-meal-plan.html#office-tuesday"
-      },
-      {
-        id: "office-wednesday",
-        name: "Upma + Chai, Chickpea Salad + Buttermilk, Lemon Rice + Pickle",
-        day: "Wednesday",
-        page: "office-meal-plan.html#office-wednesday"
-      },
-      {
-        id: "office-thursday",
-        name: "Bread + Avocado Spread, Veg Burrito Bowl + Curd, Dal Khichdi + Ghee",
-        day: "Thursday",
-        page: "office-meal-plan.html#office-thursday"
-      },
-      {
-        id: "office-friday",
-        name: "Poha + Sprouts, Jeera Rice + Rajma, Chapati + Bhindi Fry",
-        day: "Friday",
-        page: "office-meal-plan.html#office-friday"
-      }
-    ],
-    budget: [
-      {
-        id: "budget-monday",
-        name: "Poha + Tea, Rice + Dal + Veg, Chapati + Mixed Sabzi",
-        day: "Monday",
-        page: "meal-prep.html#budget-monday"
-      },
-      {
-        id: "budget-tuesday",
-        name: "Idli + Chutney, Veg Pulao + Raita, Chapati + Paneer Bhurji",
+  ]
+}
         day: "Tuesday",
         page: "meal-prep.html#budget-tuesday"
       },
